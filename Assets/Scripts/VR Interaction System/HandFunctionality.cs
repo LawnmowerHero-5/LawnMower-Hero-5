7fa688138f28dbfc2d88384e33dc27036a01a941
MathiasReitan <mathiasreitan1@gmail.com>
2021-11-30 14:32:09 +0100

    Pickup object fix, and started work on respawn object script
    Created scripts:
    RespawnAble - contains settings for when the object will respawn, and where it will respawn *not fully implemented yet, but stasble(
    
    Modified scripts:
    HandFunctionality - Fixed pickup functionality
    PickupAble - Fixed pickup funtionality (added option of breakforce required for a object to break grip on collision so it cannot move through walls)
    
    Deleted scripts:
    SnapToTransform - was not needed anymore, and will be replaced by the respawn-object-script

--- Assets/Scripts/VR Interaction System/HandFunctionality.cs
+++ Assets/Scripts/VR Interaction System/HandFunctionality.cs
@@ -3,42 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(SphereCollider))]
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(FixedJoint))]
 [RequireComponent(typeof(HandController))]
 public class HandFunctionality : MonoBehaviour
 {
-    //[HideInInspector]
-    public PickupAble currentlyHeldPickupAble;
-    
+    /*
+    Makes the controller be able to read input from HandController script, and supports following features:
+    * Pick up objects with the pickUpAble script (and they will break off if you try to move them through walls)
+    */
+    public PickupAble CurrentlyHeldPickupAble { private get; set; }
     private List<PickupAble> _pickupAblesInTriggerRadius = new List<PickupAble>();
-    
+
+    public FixedJoint FixedJoint { get; private set; }
     private HandController _handController;
 
     private void Awake()
     {
+        FixedJoint = GetComponent<FixedJoint>();
         _handController = GetComponent<HandController>();
     }
 
     private void Update()
     {
-        //Pick up pickupable if not null
+        //Pick up pickupAble if not null
         if (_handController.IsTriggerStateDown)
         {
-            print("trigger state DOWN");
-            PickupAble closestPickupAble = CheckGetClosestPickupAble();
-            if (closestPickupAble != null)
-            { 
-                closestPickupAble.AttachToController(this);
-            }
+            AttachPickupAbleToController(CheckGetClosestPickupAble());
         }
-        
-        //Drop held pickupable if not null
+        //Drop held pickupAble if not null
         else if (_handController.IsTriggerStateUp)
         {
-            print("Trigger state UP");
-            if (currentlyHeldPickupAble != null)
-            {
-                currentlyHeldPickupAble.DetachFromController(this, _handController.BehaviourPose);
-            }
+            DetachPickupAbleFromController(CurrentlyHeldPickupAble);
         }
     }
 
@@ -60,6 +56,12 @@ public class HandFunctionality : MonoBehaviour
         }
     }
 
+    private void OnJointBreak(float breakForce)
+    {
+        //Create new fixedjoint if the last one breaks
+        FixedJoint = gameObject.AddComponent<FixedJoint>();
+    }
+
     private void OnValidate()
     {
         //The spherecollider must be a trigger to prevent actual collisions
@@ -68,6 +70,13 @@ public class HandFunctionality : MonoBehaviour
         {
             sc.isTrigger = true;
             Debug.Log($"SphereCollider in {gameObject.name} must be a trigger. Automatically set to true by script");
+        }
+        //The Rigidbody on the controllers must be kinematic to prevent whacky physics when objects are picked up
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb.isKinematic == false)
+        {
+            rb.isKinematic = true;
+            Debug.Log($"Rigidbody in {gameObject.name} must be kinematic to prevent whacky physics. Automatically set to true in script");
         }
     }
 
@@ -78,9 +87,9 @@ public class HandFunctionality : MonoBehaviour
             return null;
         }
 
+        //find closest pickupAble from pickupAble withing trigger list
         float closestDistance = float.MaxValue;
         PickupAble closestPickupAble = null;
-
         foreach (PickupAble pickupAble in _pickupAblesInTriggerRadius)
         {
             if (pickupAble == null)
@@ -97,5 +106,43 @@ public class HandFunctionality : MonoBehaviour
         }
 
         return closestPickupAble;
+    }
+
+    private void AttachPickupAbleToController(PickupAble pickupAble)
+    {
+        if (pickupAble == null)
+            return;
+        
+        //Detach pickupable from already held hand if it is already held
+        if (pickupAble.currentHeldByHand != null)
+        {
+            DetachPickupAbleFromController(pickupAble);
+        }
+        
+        //Attach PickupAble to controller fixed joint
+        pickupAble.transform.position = transform.position;
+        FixedJoint.connectedBody = pickupAble.RigidBody;
+        FixedJoint.breakTorque = pickupAble.BreakForceToDetach;
+
+        //Set held values
+        CurrentlyHeldPickupAble = pickupAble;
+        pickupAble.currentHeldByHand = this;
+    }
+
+    private void DetachPickupAbleFromController(PickupAble pickupAble)
+    {
+        if (pickupAble == null)
+            return;
+        
+        //Detach from controller fixed joint
+        pickupAble.currentHeldByHand.FixedJoint.connectedBody = null;
+        
+        //Apply velocity of controllers
+        pickupAble.RigidBody.velocity = _handController.BehaviourPose.GetVelocity();    //add check for when velocity does not need to be calculated
+        pickupAble.RigidBody.angularVelocity = _handController.BehaviourPose.GetAngularVelocity();
+        
+        //Clear held values
+        pickupAble.currentHeldByHand.CurrentlyHeldPickupAble = null;
+        pickupAble.currentHeldByHand = null;
     }
 }
