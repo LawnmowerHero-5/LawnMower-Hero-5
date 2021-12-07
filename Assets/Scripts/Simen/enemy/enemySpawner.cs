b0f4a9d5d0857773c91655522977f5d3c4aa3feb
Synneogbjo <91120023+Synneogbjo@users.noreply.github.com>
2021-12-07 17:44:09 +0100

    Added Pooling & Gnome Spawner

--- Assets/Scripts/Simen/enemy/enemySpawner.cs
+++ Assets/Scripts/Simen/enemy/enemySpawner.cs
@@ -1,9 +1,20 @@
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class enemySpawner : MonoBehaviour
 {
-    private ObjectPooler pooler;
+    [SerializeField] private int minSpawnCount = 1;
+    [SerializeField] private int maxSpawnCount = 3;
+    [SerializeField] private BoxCollider spawnTrigger;
+    [SerializeField] private BoxCollider exitSpawnTrigger;
 
+    [HideInInspector] public bool triggerEnter;
+    [HideInInspector] public bool triggerExit;
+    [HideInInspector] public Collider triggerCollider;
+    
+    private ObjectPooler pooler;
+    private bool canSpawn = true;
+    
     private void Start()
     {
         pooler = ObjectPooler.Instance;
@@ -11,6 +22,41 @@ public class enemySpawner : MonoBehaviour
 
     private void FixedUpdate()
     {
-        pooler.SpawnFromPool("Gnome", transform.position + new Vector3(0f, 2f, 0f), Quaternion.identity);
+        if (triggerEnter) EnteredTrigger(triggerCollider);
+        else if (triggerExit) ExitedTrigger();
+    }
+
+    private void EnteredTrigger(Collider other)
+    {
+        triggerEnter = false;
+
+        if (!canSpawn || pooler.pools[0].activeObjects >= pooler.pools[0].size)
+        {
+            triggerCollider = null;
+            return;
+        }
+            
+        canSpawn = false;
+        var count = Random.Range(minSpawnCount, maxSpawnCount);
+        
+        for (var i = 0; i < count; i++)
+        {
+            pooler.SpawnFromPool("gnome",
+                transform.position + new Vector3(Random.Range(-0.5f, 0.5f), 2f, Random.Range(-0.5f, 0.5f)),
+                Quaternion.identity);
+            
+            pooler.pools[0].activeObjects++;
+            
+            if (pooler.pools[0].activeObjects >= pooler.pools[0].size) break; //Exits loop if all objects in pool are activated
+        }
+        
+        triggerCollider = null;
+    }
+
+    private void ExitedTrigger()
+    {
+        print("EXIT");
+        triggerExit = false;
+        canSpawn = true;
     }
 }
