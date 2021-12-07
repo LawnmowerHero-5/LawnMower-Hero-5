7065f366b92474bf8a6e7c0e3cfd4f89bb1b6740
Synneogbjo <91120023+Synneogbjo@users.noreply.github.com>
2021-12-07 14:03:56 +0100

    Added pooling

--- Assets/Scripts/Simen/player/playerPosition.cs
+++ Assets/Scripts/Simen/player/playerPosition.cs
@@ -1,26 +1,11 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class playerPosition : MonoBehaviour
 {
     public transformVariable trans;
-    public Rigidbody rb;
-    private playFabManager _playFabManager;
-    private scoreController _scoreController;
-
-    private void Start()
-    {
-        _playFabManager = GetComponent<playFabManager>();
-        _scoreController = GetComponent<scoreController>();
-    }
 
     private void Update()
     {
         trans.playerTransform = transform;
     }
-
-    
 }
