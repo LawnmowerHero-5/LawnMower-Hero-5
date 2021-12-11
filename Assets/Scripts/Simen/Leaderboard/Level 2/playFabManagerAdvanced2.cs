6efba13380f2e01be082727f983256e9f4b0e05d
MathiasReitan <mathiasreitan1@gmail.com>
2021-12-11 21:47:43 +0100

    Timer and Ui now works with everything

--- Assets/Scripts/Simen/Leaderboard/Level 2/playFabManagerAdvanced2.cs
+++ Assets/Scripts/Simen/Leaderboard/Level 2/playFabManagerAdvanced2.cs
@@ -40,7 +40,10 @@ public class playFabManagerAdvanced2 : MonoBehaviour
     private void Start()
     {
         nameWindow.SetActive(false);
-        leaderboardWindow.SetActive(false);
+        if (leaderboardWindow != null)
+        {
+            leaderboardWindow.SetActive(false);
+        }
         _timer = GetComponent<Timer>();
         _scoreController = GetComponent<scoreManager>();
         _pMenu = GetComponent<pauseEffect>();
@@ -132,6 +135,8 @@ public class playFabManagerAdvanced2 : MonoBehaviour
 
     void OnLeaderoardGet(GetLeaderboardResult result)
     {
+        if (rowParent == null) return;
+
         foreach (Transform item in rowParent)
         {
             Destroy(item.gameObject);
@@ -163,6 +168,8 @@ public class playFabManagerAdvanced2 : MonoBehaviour
     
     void OnFirstPlaceGet(GetLeaderboardResult result)
     {
+        if (firstPlace == null) return;
+
         foreach (Transform item in firstPlace)
         {
             Destroy(item.gameObject);
@@ -227,6 +234,7 @@ public class playFabManagerAdvanced2 : MonoBehaviour
         PlayFabClientAPI.UpdateUserTitleDisplayName(request, OnDisplayNameUpdate, OnError);
         PullUpLeaderboard();
         _pMenu.Resume();
+        Time.timeScale = 1f;
         _sceneController.LoadScene("MainMenu");
     }
 
@@ -252,12 +260,17 @@ public class playFabManagerAdvanced2 : MonoBehaviour
     public void SetYourName()
     {
         nameWindow.SetActive(true);
-        leaderboardWindow.SetActive(false);
-    }
+        if (leaderboardWindow != null)
+        {
+            leaderboardWindow.SetActive(false);
+        }    }
 
     public void PullUpLeaderboard()
     {
         nameWindow.SetActive(false);
-        leaderboardWindow.SetActive(true);
+        if (leaderboardWindow != null)
+        {
+            leaderboardWindow.SetActive(true);
+        }
     }
 }
