f4467d5a1e468f9bb6382eccf5f98ebb7ba0a23d
Synneogbjo <91120023+Synneogbjo@users.noreply.github.com>
2021-12-11 20:03:35 +0100

    saves prefs on scene change

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,3 +1,4 @@
+using System;
 using PlayerPreferences;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -5,20 +6,42 @@ using UnityEngine.SceneManagement;
 public class SceneController : MonoBehaviour
 {
     [SerializeField] private DataController _Data;
+    private float timer;
+    private string action;
+    private string targetScene;
+
+    public static event Action swappedScene;
     
     public void LoadScene(string sceneName)
     {
         _Data.SetPlayerData();
-        SceneManager.LoadScene(sceneName);
+        targetScene = sceneName;
+        timer = 0.05f;
+        action = "load";
     }
     
     public void ResetScene()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        _Data.SetPlayerData();
+        timer = 0.05f;
+        action = "reset";
     }
 
     public void QuitGame()
     {
-        Application.Quit();
+        _Data.SetPlayerData();
+        timer = 0.05f;
+        action = "quit";
     }
+
+    private void FixedUpdate()
+    {
+        if (timer > 0) timer -= Time.fixedDeltaTime;
+        else
+        {
+            if (action == "load") SceneManager.LoadScene(targetScene);
+            else if (action == "quit") Application.Quit();
+            else SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
+}
