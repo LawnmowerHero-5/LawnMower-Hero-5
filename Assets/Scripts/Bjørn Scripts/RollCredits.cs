705565a2169f59fec47ab8fd85e984ecb2b51413
Synneogbjo <91120023+Synneogbjo@users.noreply.github.com>
2021-12-12 14:34:51 +0100

    Updated end Credits

--- Assets/Scripts/Bjørn Scripts/RollCredits.cs
+++ Assets/Scripts/Bjørn Scripts/RollCredits.cs
@@ -4,11 +4,22 @@ public class RollCredits : MonoBehaviour
 {
     [SerializeField] private float scrollSpd = 60f;
     [SerializeField] private float endOfCredits;
+    [SerializeField] private float returnTime = 4f;
+    private float timer;
 
-    // Update is called once per frame
-    void Update()
+    private SceneController _Scene;
+
+    private void Start()
+    {
+        timer = returnTime;
+
+        _Scene = GetComponent<SceneController>();
+    }
+    
+    private void Update()
     {
         if (transform.localPosition.y <= endOfCredits) transform.localPosition += new Vector3(0f, scrollSpd * Time.deltaTime, 0f);
-        else print("End");
+        else if (timer > 0) timer -= Time.deltaTime;
+        else _Scene.LoadScene("MainMenu");
     }
 }
